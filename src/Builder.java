baab0d5c87812996f5fc28948fd3d982b700fcdc
massoniwe <chjonathan@yandex.ru>
2026-09-12 15:52:26 +0500

    feat: Add ChefDirector and Main execution

--- src/Builder.java
+++
@@ -1,6 +0,0 @@
-public interface Builder {
-    Builder setCarType(String carType);
-    Builder setSeats(int seats);
-    Builder setEngine(String engine);
-    Builder setGPS(boolean hasGPS);
-}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,4 @@
+package PACKAGE_NAME;
+
 public class Main {
-    public static void main(String[] args) {
-        Director director = new Director();
-        CarBuilder builder = new CarBuilder();
-
-        director.constructSportsCar(builder);
-        Car sportsCar = builder.build();
-        System.out.println("Из директора: " + sportsCar);
-
-        director.constructSUV(builder);
-        Car suv = builder.build();
-        System.out.println("Из директора: " + suv);
-    }
 }
-
